c94278cf9da19d112423b5a2ffee105296d6e9e4
Qelemnaz90 <enazy1990@gmail.com>
2024-03-03 14:29:14 +0400

    index

---
+++ index.js
@@ -0,0 +1,6 @@
+var age = prompt("Enter your age: ")
+var a = 18-age
+
+
+if (age >= 18 )  { alert  ("You are old enough to drive.")}
+else if(age < 18 )  {alert ("You are left with"  +  a  +  "years to drive.")}
